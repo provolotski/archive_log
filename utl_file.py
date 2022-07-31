7d708553e49bea1c13d614d926accee2a920019f
provolotski <provolotski@gmail.com>
2022-07-31 11:51:13 +0300

    обработка командной строки для добавления нового события

--- utl_file.py
+++ utl_file.py
@@ -1,9 +1,7 @@
-
+ # потом возможно уберу, но пока для работы с файлами по сети
 import os
 import subprocess
 
-
-# потом возможно уберу, но пока для работы с файлами по сети
 def move_to_server(file, user, server, path ):
     cmd = 'scp {file} {user}@{server}:{path}'.format(file=file, user=user, server=server, path=path)
     subprocess.call(cmd, shell=True)
